7ea7a71169b287cc5afb93b4dc1dcabd07d94d9e
Alexander Koller <koller@ling.uni-potsdam.de>
2016-06-15 19:52:06 +0200

    show_participants would work

--- helpers.py
+++ helpers.py
@@ -1,15 +1,21 @@
 from jinja2 import evalcontextfilter
 
 from __init__ import *
+from tables import *
 
 @app.template_filter()
 @evalcontextfilter
 def ft(eval_ctx, value, format='%Y-%m-%d %H:%M'):
     return value.strftime(format)
 
-_parts = ["--", "Tn", "Ld", "Br", "Bs"]
+parts = ["--", "Tn", "Ld", "Br", "Bs"]
+lparts = ["None", "Tenor", "Lead", "Baritone", "Bass"]
 
 @app.template_filter()
 @evalcontextfilter
 def part(eval_ctx, value):
-    return _parts[value]
+    return parts[value]
+
+
+
+countries = {country.id: country for country in session.query(Country).all()}
--- show_participants.py
+++ show_participants.py
@@ -1,9 +1,46 @@
 
 from __init__ import *
 from tables import *
-
+from itertools import groupby
+from helpers import *
 
 @app.route("/show-participants.html")
 def show_participants():
     participants = session.query(Participant).all()
-    return render_template("show_participants.html", title="Show participants", participants=participants)
+    paid_participants = session.query(Participant).filter(Participant._paypal_status == 5)
+    total_donations = sum([p.donation for p in participants])
+
+    return render_template("show_participants.html", title="Show participants",
+                           participants=participants, total_donations=total_donations,
+                           part_data=make_part_data(paid_participants), country_data=make_country_data(paid_participants))
+
+
+
+
+
+
+part_colors = ["428BCA", "5CB85C", "F0AD4E", "D9534F"]
+part_highlight_colors = ["5DA6E6", "78D077", "FCC671", "E16864"]
+
+country_colors = ["42847D", "615192", "D4C26A", "D4976A", "428BCA", "5CB85C", "F0AD4E", "D9534F"]
+country_highlight_colors = ["6A9E99", "887CAF", "FFF0AA", "FFCEAA", "5DA6E6", "78D077", "FCC671", "E16864"]
+
+
+def participants_by(all_participants, fn):
+    s = sorted(all_participants, key=fn)
+    return groupby(s, fn)
+
+
+def make_part_data(all_participants):
+    p_by_part = participants_by(all_participants, lambda p: p.part1)
+    return ["{ value: %d, color:'#%s', highlight:'#%s', label:'%s'}" %
+            (len(list(people)), part_colors[i], part_highlight_colors[i], lparts[part])
+            for (i, (part,people)) in enumerate(p_by_part)]
+
+
+def make_country_data(all_participants):
+    p_by_country = participants_by(all_participants, lambda p: p.country.id)
+    return ["{ value: %d, color:'#%s', highlight:'#%s', label:'%s'}" %
+            (len(list(people)), country_colors[i], country_highlight_colors[i], countries[country].name_en)
+            for (i, (country,people)) in enumerate(p_by_country)]
+
